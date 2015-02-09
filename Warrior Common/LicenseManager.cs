b161897a52b39fc01d08d411616dbc9b384f864f
Eric Selin <eric.selin@gmail.com>
2015-02-10 00:58:39 +0200

    Fixed bug with anonymous types in license key check

--- Warrior Common/LicenseManager.cs
+++ Warrior Common/LicenseManager.cs
@@ -14,28 +14,35 @@ namespace WarriorCommon
         public string Company;
         public DateTime ValidUntil;
     }
+	public class LicenseInfo
+	{
+		public string email;
+		public string licenseKey;
+	}
 
-    public static class LicenseManager
-    {
-        private const string APPLICATION_KEY = "VezvNeMAfhGkpCKoEraaHeTmbzNSFM47";
-        public static async Task<License> CheckLicense(string email, string licenseKey)
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("https://ppwarrior.azure-mobile.net/");
-                // Add an Accept header for JSON format.
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                // add application key
-                client.DefaultRequestHeaders.Add("X-ZUMO-APPLICATION", APPLICATION_KEY);
+	public static class LicenseManager
+	{
+		private const string APPLICATION_KEY = "VezvNeMAfhGkpCKoEraaHeTmbzNSFM47";
+		public static async Task<License> CheckLicense(string email, string licenseKey)
+		{
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri("https://ppwarrior.azure-mobile.net/");
+				// Add an Accept header for JSON format.
+				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+				// add application key
+				client.DefaultRequestHeaders.Add("X-ZUMO-APPLICATION", APPLICATION_KEY);
 
-                HttpResponseMessage response = await client.PostAsJsonAsync("api/checkLicense", new { email = email, licenseKey = licenseKey });
-                if (response.IsSuccessStatusCode)
-                {
-                    return await response.Content.ReadAsAsync<License>();
-                }
+				var licenseInfo = new LicenseInfo { email = email, licenseKey = licenseKey };
 
-                return null;
-            }
-        }
-    }
+				HttpResponseMessage response = await client.PostAsJsonAsync("api/checkLicense", licenseInfo);
+				if (response.IsSuccessStatusCode)
+				{
+					return await response.Content.ReadAsAsync<License>();
+				}
+
+				return null;
+			}
+		}
+	}
 }
